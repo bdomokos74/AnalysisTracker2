61f936888f624ab2a3fde9b2a4121ee865e686a2
Balint Domokos <bdomokos74@gmail.com>
2013-06-20 13:39:59 +0200

    Project created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AnalysisTracker2::Application.config.secret_token = '67cb540925f8e9847ee215168e155457dc00e74fe67ebfb7c862666e65db312bd38463c3bf0b5016c496282e056f2d77e83a880ffa3f3fe930ad58162c699646'
